4bb541944edab8ed600be724552ee529a8790d65
Xanonymous <trusaidlin@gmail.com>
2025-09-01 23:00:39 +0800

    feat: 🎸 setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button.tsx";
+
 function App() {
-  return <h1>Vite + React</h1>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
